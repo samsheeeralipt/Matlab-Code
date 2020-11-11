ad8bfb4bee772435a50f0c4291ece002719556af
samsheeeralipt <58585707+samsheeeralipt@users.noreply.github.com>
2020-11-11 12:49:35 +0530

    Add files via upload
    This code  is used to create a line scan of an image around a circle with a given radius. This can be used to get the pixel along a circular path in an image.

---
+++ AzimuthalLineScan.m
@@ -0,0 +1,18 @@
+function [p]=AzimuthalLineScan(image,radius,data_points)
+
+[A,B] = size(image);
+%N=400;
+angles=linspace(0,2*pi,data_points);
+%r=200;
+x0=round(A/2);y0=round(B/2);
+x=radius*sin(angles)+x0;
+y=radius*cos(angles)+y0;
+X=round(x);Y=round(y);
+p=zeros(1,data_points);k=1;
+for m=1:data_points
+        k0=X(m);
+        l0=Y(m);
+   p(k)=image(k0,l0);
+   k=k+1;
+end
+fig=plot(p);fig(1).LineWidth = 2;
